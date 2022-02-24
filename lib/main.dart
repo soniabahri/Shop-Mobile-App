e4b1cb2498d1a1634a7ee8597678a9ea786c7fc9
soniabahri <soniabahri417@gmail.com>
2022-02-24 22:06:40 +0100

    Application mobile with flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:html';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
@@ -9,13 +12,24 @@ class ShopApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return MyBody();
+  }
+
+  // ignore: non_constant_identifier_names
+  MaterialApp MyBody() {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.deepOrange,
-          title: const Text('Shopping Time !'),
+        body: Container(
+          color: const Color.fromRGBO(230, 230, 230, 99.9),
+          child: Column(
+            children: const [
+              Text(
+                'Welcome to your Shopping App',
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
         ),
-        body: const Center(),
       ),
     );
   }
