84f84feb6ff66f56a6c0060880a2d54cf7a2747b
soniabahri <soniabahri417@gmail.com>
2022-02-24 23:19:32 +0100

    Application mobile with flutter

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 void main(List<String> args) {
   runApp(const ShopApp());
+  runApp(const MainPage());
 }
 
 class ShopApp extends StatelessWidget {
@@ -25,9 +26,43 @@ class ShopApp extends StatelessWidget {
                 textAlign: TextAlign.center,
               ),
               Image.asset("assets/images/shopping-online.png"),
+              ElevatedButton(
+                onPressed: onPressed,
+                child: const Text("Start"),
+              )
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  void onPressed() {
+    // ignore: prefer_typing_uninitialized_variables
+    var context2;
+    Navigator.push(
+      context2,
+      MaterialPageRoute(
+        builder: (context) => const MainPage(),
+      ),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color.fromRGBO(148, 118, 163, 99.9),
+          title: const Text(
+            "Cake",
+            textAlign: TextAlign.center,
+          ),
+        ),
+        body: const Text("Testing !"),
       ),
     );
   }
