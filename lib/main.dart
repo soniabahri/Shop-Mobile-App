5d3eb7e59fdddf6200bc2a34cd030895ca96f722
soniabahri <soniabahri417@gmail.com>
2022-02-25 19:20:33 +0100

    Mobile Application made with flutter

--- lib/main.dart
+++ lib/main.dart
@@ -63,7 +63,7 @@ class MainPage extends StatelessWidget {
             
           ),
         ),
-        body: const Text("Testing !"),
+        body: const Text("Testing Time"),
       ),
     );
   }
