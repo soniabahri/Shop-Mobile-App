c33560927a0bf5ba3754534172bc5ea1e7e4e150
soniabahri <soniabahri417@gmail.com>
2022-02-28 19:53:05 +0100

    Mobile Application made with Flutter

--- lib/models/Product.dart
+++ lib/models/Product.dart
@@ -1,1 +1,25 @@
 import 'package:flutter:material.dart';
+
+class Product { 
+    final String image , title , description ;
+    final int price , size , id ;
+    final Color color ; 
+    Product ({ 
+        this.id;
+        this.image;
+        this.title;
+        this.price;
+        this.description;
+        this.size;
+        this.color;
+
+
+
+
+
+    })
+
+
+
+
+}
