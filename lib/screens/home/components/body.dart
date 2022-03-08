2dbdea64e3620cd77f58ea9166f5eb6ae36eb64d
soniabahri <soniabahri417@gmail.com>
2022-03-08 21:46:07 +0100

    -

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -81,7 +81,7 @@ class ItemCard extends StatelessWidget {
           ),
         ),
         const Text(
-          "100 TND",
+          "100 .TND",
           style: const TextStyle(fontWeight: FontWeight.bold),
         )
       ],
