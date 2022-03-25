f42dc93e3d709cd862a44461fda6bcaeb8e20d3f
soniabahri <soniabahri417@gmail.com>
2022-03-25 21:39:09 +0100

    product details

---
+++ lib/screens/home/components/item_card.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import '../../../constants.dart';
+import '../../../models/Product.dart';
+
+class ItemCard extends StatelessWidget {
+  final Product product;
+  //final Function press;
+  const ItemCard({
+    Key? key,
+    required this.product,
+    //required this.press,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Expanded(
+          child: Container(
+            padding: const EdgeInsets.all(kDefalutPaddin),
+            // height: 180,
+            // width: 160,
+            decoration: BoxDecoration(
+              color: product.color,
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: Image.asset(product.image),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: kDefalutPaddin / 4),
+          child: Text(
+            product.title,
+            style: const TextStyle(color: kTextLightColor),
+          ),
+        ),
+        Text(
+          "\$${product.price}",
+          style: const TextStyle(fontWeight: FontWeight.bold),
+        )
+      ],
+    );
+  }
+}
